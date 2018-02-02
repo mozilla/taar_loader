477846f9c9dbbaba4729252c018abfbf5d2e1ce1
Victor Ng <victor@crankycoder.com>
2018-02-01 21:14:13 -0500

    added more metadata to support PyPI

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='taar_loader',
       description='Migration tool to move data from Telemetry S3 buckets over to DynamoDB',
       author='Victor Ng',
       author_email='vng@mozilla.com',
+      url='https://github.com/crankycoder/taar_loader',
       license='MPL 2.0',
       packages=['taar_loader'])
